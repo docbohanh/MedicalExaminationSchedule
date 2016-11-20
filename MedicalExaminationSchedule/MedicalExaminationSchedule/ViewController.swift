4ef1e4ab6b0c05825c4a740d5451f495333b6c0a
lighthouse2403 <haidang2403@gmail.com>
2016-11-20 12:45:37 +0700

    make project

---
+++ MedicalExaminationSchedule/MedicalExaminationSchedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedicalExaminationSchedule
+//
+//  Created by Nguyen Hai Dang on 11/20/16.
+//  Copyright © 2016 Nguyen Hai Dang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
