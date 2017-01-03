9262b22dcdca60812f456d577ce308848b34e9ad
lighthouse2403 <haidang2403@gmail.com>
2017-01-03 10:45:36 +0700

    no message

---
+++ MedicalExaminationSchedule/MedicalExaminationSchedule/More/DocumentViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DocumentViewController.swift
+//  MedicalExaminationSchedule
+//
+//  Created by Hai Dang Nguyen on 1/3/17.
+//  Copyright © 2017 Nguyen Hai Dang. All rights reserved.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+
+    @IBOutlet weak var webview: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
